e9716b46702bd8b60432229ff5818066a32a4261
lixingzh <lixing.zhang@outlook.com>
2018-01-31 23:24:28 -0700

    [design-pattern-examples] added iterator pattern, composite pattern.

---
+++ compositepattern/src/main/java/com/lixingzh/java/designpattern/CompositePatternMain.java
@@ -0,0 +1,13 @@
+package com.lixingzh.java.designpattern;
+
+/**
+ * Created by magicxdms on 1/31/18.
+ */
+
+/**
+ * Composite Pattern
+ * Allows you to compose objecs into tree structures to represent part-whole hierarchies.
+ * Composite lets clients treat individual objects and compositions of objects uniformly
+ */
+public class CompositePatternMain {
+}
